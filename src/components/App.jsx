ea56bbfd2bc95eb46073fdfb3865718bcdeae4ab
Yuli <yu.expert@gmail.com>
2024-11-10 00:26:54 +0200

    Add task 1

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import Profile from './Profile/Profile';
+import userData from '../userData.json';
+import 'modern-normalize';
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,38 @@
+// import React from 'react';
+import s from './Profile.module.css';
+
+const Profile = ({
+  name,
+  tag,
+  location,
+  image = 'https://cdn-icons-png.flaticon.com/512/1077/1077012.png',
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <div className={s.wrapper}>
+      <div className={s.user}>
+        <img className={s.img} src={image} alt="User avatar" />
+        <p className={s.name}>{name}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
+
+      <ul className={s.userList}>
+        <li className={s.listItem}>
+          <span>Followers</span>
+          <span className={s.bold}>{followers}</span>
+        </li>
+        <li className={s.listItem}>
+          <span>Views</span>
+          <span className={s.bold}>{views}</span>
+        </li>
+        <li className={s.listItem}>
+          <span>Likes</span>
+          <span className={s.bold}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
